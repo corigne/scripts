21ffacd73140fd84461f7cb95df5681e6116b113
nathan j. <corigne@gmail.com>
2024-02-28 22:11:03 -0600

    Removed scp call from darksouls backup script. Using unison to sync on a cron job instead.

--- darksouls_proton_save_backup.sh
+++ darksouls_proton_save_backup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-BAK_DIR=$HOME/games/backups/DarkSouls
+BAK_DIR=$HOME/Games/backups/DarkSouls
 
 #Backs up dark souls 2 and 3 save files.
 [ -d $BAK_DIR ] || mkdir -p $BAK_DIR && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
 
 cp $HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII/0110000104dfa8c1/* $HOME/backup_saves
 cp $HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII/0110000104dfa8c1/* $HOME/backup_saves
-scp $HOME/backup_saves/* nexus@misfits.rip:Documents/backup_saves
 
 echo 'Done backing up saves...'
 echo
