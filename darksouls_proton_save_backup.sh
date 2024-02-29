b1f0f675495d792c747f85cce421a0118a5073ba
nathan j. <corigne@gmail.com>
2024-02-28 21:58:48 -0600

    Clerical changes to dark souls backup script.

--- darksouls_proton_save_backup.sh
+++ darksouls_proton_save_backup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+BAK_DIR=$HOME/games/backups/DarkSouls
+
 #Backs up dark souls 2 and 3 save files.
-[ -d $HOME/backup_saves ] || (mkdir $HOME/backup_savesi && echo 'Creating backup save directory.')
+[ -d $BAK_DIR ] || mkdir -p $BAK_DIR && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
+
 cp $HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII/0110000104dfa8c1/* $HOME/backup_saves
 cp $HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII/0110000104dfa8c1/* $HOME/backup_saves
 scp $HOME/backup_saves/* nexus@misfits.rip:Documents/backup_saves
