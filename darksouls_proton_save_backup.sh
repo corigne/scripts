a01ba19419ecdc9fdc89b2193d588315d5dda1ec
Nathan Jodoin <corigne@gmail.com>
2024-02-28 22:25:38 -0600

    Fixed logic error is ds backup and added ds backup restore script.

--- darksouls_proton_save_backup.sh
+++ darksouls_proton_save_backup.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
+#Backs up dark souls 2 and 3 save files.
 
 BAK_DIR=$HOME/Games/backups/DarkSouls
+DS2_SRC=$HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII
+DS3_SRC=$HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII
 
-#Backs up dark souls 2 and 3 save files.
 [ -d $BAK_DIR ] || mkdir -p $BAK_DIR && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
+[ -d $BAK_DIR/DS2 ] || mkdir -p $BAK_DIR/DS2 && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
+[ -d $BAK_DIR/DS3 ] || mkdir -p $BAK_DIR/DS3 && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
 
-cp $HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII/0110000104dfa8c1/* $HOME/backup_saves
-cp $HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII/0110000104dfa8c1/* $HOME/backup_saves
+cp -r $DS2_SRC/* $BAK_DIR/DS2
+cp -r $DS3_SRC/* $BAK_DIR/DS3
 
 echo 'Done backing up saves...'
 echo
---
+++ darksouls_proton_save_restore.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#restores backup saves to their correct locations, if the prefix already exists
+
+BAK_DIR=$HOME/Games/backups/DarkSouls
+DS2_SRC=$HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII
+DS3_SRC=$HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII
+
+#Backs up dark souls 2 and 3 save files.
+[ -d $BAK_DIR ] || echo "Backup directory does not exist to restore from." && return 1
+
+cp -r $BAK_DIR/DS2/* $DS2_SRC
+cp -r $BAK_DIR/DS3/* $DS3_SRC
+
+echo 'Done restoring saves...'
+echo
