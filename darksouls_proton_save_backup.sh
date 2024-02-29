63d25f874495b94da30078e40c51c85a13411f2d
nathan j. <corigne@gmail.com>
2024-02-28 22:31:17 -0600

    Addition ds script changes.

--- darksouls_proton_save_backup.sh
+++ darksouls_proton_save_backup.sh
@@ -5,9 +5,9 @@ BAK_DIR=$HOME/Games/backups/DarkSouls
 DS2_SRC=$HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII
 DS3_SRC=$HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII
 
-[ -d $BAK_DIR ] || mkdir -p $BAK_DIR && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
-[ -d $BAK_DIR/DS2 ] || mkdir -p $BAK_DIR/DS2 && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
-[ -d $BAK_DIR/DS3 ] || mkdir -p $BAK_DIR/DS3 && echo "Backup directory did not exist. Created backup directory: $BAK_DIR."
+[ -d $BAK_DIR ] || (mkdir -p $BAK_DIR && echo "Backup directory did not exist. Created backup directory: $BAK_DIR.")
+[ -d $BAK_DIR/DS2 ] || (mkdir -p $BAK_DIR/DS2 && echo "Backup directory did not exist. Created backup directory: $BAK_DIR.")
+[ -d $BAK_DIR/DS3 ] || (mkdir -p $BAK_DIR/DS3 && echo "Backup directory did not exist. Created backup directory: $BAK_DIR.")
 
 cp -r $DS2_SRC/* $BAK_DIR/DS2
 cp -r $DS3_SRC/* $BAK_DIR/DS3
--- darksouls_proton_save_restore.sh
+++ darksouls_proton_save_restore.sh
@@ -6,7 +6,7 @@ DS2_SRC=$HOME/.steam/root/steamapps/compatdata/335300/pfx/drive_c/users/steamuse
 DS3_SRC=$HOME/.steam/root/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII
 
 #Backs up dark souls 2 and 3 save files.
-[ -d $BAK_DIR ] || echo "Backup directory does not exist to restore from." && return 1
+[ -d $BAK_DIR ] || echo "Backup directory does not exist to restore from."
 
 cp -r $BAK_DIR/DS2/* $DS2_SRC
 cp -r $BAK_DIR/DS3/* $DS3_SRC
