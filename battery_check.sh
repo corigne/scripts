c296f03df2e94a614253ccbb36bb0141a051b469
Nathan Jodoin <corigne@gmail.com>
2024-04-06 14:44:08 -0500

    updated battery and touchpade scripts, renamed touchpad script

--- battery_check.sh
+++ battery_check.sh
@@ -1,9 +1,51 @@
 #!/bin/sh
-acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
-	read -r status capacity
 
-	if [ "$status" = Discharging -a "$capacity" -lt 7 ]; then
+BAT_LOW=15
+BAT_CRITICAL=5
+
+if [ "$1" = "--help" ]
+then
+    printf "
+    Usage:
+    \tbattery_check.sh warning%% hibernate%%
+
+    Description:
+    \tA script for notifying the user via dunst and logging when
+    \tthe battery is low and the system is going to hibernate.
+    \tCan be supplied arguments for the battery low warning and
+    \thibernation percentage thresholds as the first and second arguments.
+
+    \t Default behavior is to warn at 15% and hibernate at 5%."
+    exit
+fi
+
+if [[ -n "$1" && -n "$2" && $1 -gt $2 ]]
+then
+    BAT_LOW=$1
+    BAT_CRITICAL=$2
+fi
+
+acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
+
+	read -r status capacity
+    echo Low threshold: $BAT_LOW, Hibernate threshold: $BAT_CRITICAL
+    echo Status: $status, Capacity: $capacity
+
+    if [ "$status" = Discharging -a "$capacity" -le $BAT_CRITICAL ]; then
+        echo Battery critical threshold.
+		dunstify -u critical "Critical battery threshold, hibernating..."
 		logger "Critical battery threshold, hibernating..."
+        sleep .5
 		systemctl hibernate
+        exit
 	fi
+
+    if [ "$status" = Discharging -a "$capacity" -le $BAT_LOW ]; then
+        echo Battery low threshold.
+		dunstify -u critical 'Battery low! System will hibernate at 5%.'
+        logger 'Battery low! System will hibernate at 5%.'
+        sleep .5
+        light -S 15
+		exit
+    fi
 }
