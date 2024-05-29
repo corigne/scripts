d35e1c5e4971fbe7d3916c6339a1fcbd8c851bbd
Nathan Jodoin <corigne@gmail.com>
2024-05-29 11:02:09 -0500

    Fixed issues with usr background folders in wrong script.

--- prepare_sfw_slideshow.zsh
+++ prepare_sfw_slideshow.zsh
@@ -1,10 +1,24 @@
 #!/bin/zsh
 setopt nonomatch
+shared_slideshow='/usr/share/backgrounds/slideshow/'
 slideshow=$HOME'/pictures/sfw/'
 pics=$HOME'/pictures/'
 dirs=($(find $pics -type d -name wallpaper -not -path "**/questionable/**"))
-echo '//////// CLEANING UP' $slideshow '////////'
+echo '//////// CLEANING UP' $slideshow and $shared_slideshow '////////'
+
 let i=0; let k=0
+for file in $shared_slideshow/*
+do
+  if [[ $i -eq 80 ]]
+  then
+    echo
+    i=0
+  fi
+  echo -n '.'
+  [ -f "$file" ] && sudo rm $file && i=i+1 && k=k+1
+done
+
+i=0; k=0
 for file in $slideshow/*
 do
   if [[ $i -eq 80 ]]
@@ -30,7 +44,7 @@ do
       i=0
     fi
     echo -n '.'
-    [ -f "$file" ] && cp $file $slideshow && i=i+1 && k=k+1
+    [ -f "$file" ] && cp $file $slideshow && sudo cp $file $shared_slideshow && i=i+1 && k=k+1
   done
   echo '\n'
   i=0
