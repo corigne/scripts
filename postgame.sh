73eaeab90cff1f778286293bb45bfb490ebe0480
Nathan Jodoin <corigne@gmail.com>
2024-05-30 10:56:36 -0500

    changed sleep times in postgame.sh

--- postgame.sh
+++ postgame.sh
@@ -2,13 +2,12 @@
 
 # Post-game, restores previous monitor configs.
 # Kills exterior monitors.
-picom -b &
 
-sleep .05
 nvidia-settings --assign CurrentMetaMode="DP-4: 2560x1440_144 {ForceCompositionPipeline=On, AllowGSYNCCompatible=On}"
-sleep .05
+sleep 0.05
 xrandr --output DP-2 --mode 2560x1440 --rate 60 --right-of DP-4
-sleep .05
+sleep 0.05
 
-$HOME/scripts/polybar_lch.sh &
+picom -b &
 $HOME/scripts/home_slideshow.sh &
+$HOME/scripts/polybar_lch.sh &
