6495369a2ac3009fe5296bb705e0b1c3faefcd54
Nathan Jodoin <corigne@gmail.com>
2024-10-22 19:08:24 -0500

    Desktop changes.

--- hyprlock.sh
+++ hyprlock.sh
@@ -4,6 +4,8 @@ PICDIR=$HOME/Pictures/sfw
 pidof hyprlock && exit 0
 
 PIC=$(ls ~/Pictures/sfw/ | shuf -n1)
+PIC2=$(ls ~/Pictures/sfw/ | shuf -n1)
 
 cp "$PICDIR/$PIC" ~/Pictures/.lock_wallpaper
+cp "$PICDIR/$PIC2" ~/Pictures/.lock_wallpaper2
 hyprlock
--- nvidia.sh
+++ nvidia.sh
@@ -7,5 +7,5 @@ do
 done
 /usr/bin/mkinitcpio -P
 
-xrandr --output DP-4 --mode 2560x1440 --rate 60 --right-of DP-2
-nvidia-settings --assign CurrentMetaMode="DP-2: 2560x1440_144 +0+0 {ForceCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-4: 2560x1440_60 +2560+0 {ForceCompositionPipeline=On, AllowGSYNCCompatible=Off}"
+# xrandr --output DP-4 --mode 2560x1440 --rate 60 --right-of DP-2
+# nvidia-settings --assign CurrentMetaMode="DP-2: 2560x1440_144 +0+0 {ForceCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-4: 2560x1440_60 +2560+0 {ForceCompositionPipeline=On, AllowGSYNCCompatible=Off}"
