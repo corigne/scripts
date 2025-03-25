f825faa3369e5b8858e651c48c45d368992e7f8a
Nathan Jodoin <corigne@gmail.com>
2025-03-24 19:45:40 -0500

    Update slideshow scripts.

--- prepare_sfw_slideshow.zsh
+++ prepare_sfw_slideshow.zsh
@@ -1,55 +1,81 @@
 #!/bin/zsh
+
 setopt nonomatch
+setopt nonomatch
+
 shared_slideshow='/usr/share/backgrounds/slideshow/'
 slideshow=$HOME'/Pictures/sfw/'
 pics=$HOME'/Pictures/'
+
 dirs=($(find $pics -type d -name wallpaper -not -path "**/questionable/**"))
 echo '//////// CLEANING UP' $slideshow and $shared_slideshow '////////'
+
+VERBOSE=false
+POS_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v||--verbose)
+      VERBOSE=true
+      shift
+      ;;
+    -*||--*)
+      echo "Unknown Option"
+      exit 1
+      ;;
+    *)
+      POS_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
 
 let i=0; let k=0
 for file in $shared_slideshow/*
 do
-  if [[ $i -eq 80 ]]
+  if [[ $i -eq ($(tput cols) - 1) ]]
   then
     echo
     i=0
   fi
-  echo -n '.'
-  [ -f "$file" ] && sudo rm $file && i=i+1 && k=k+1
+  echo -n '-'
+  [ -f "$file" ] && sudo rm $file && i=i+1 && k=k+1 && printf "-"
 done
 
 i=0; k=0
 for file in $slideshow/*
 do
-  if [[ $i -eq 80 ]]
+  if [[ $i -eq ($(tput cols) - 1) ]]
   then
     echo
     i=0
   fi
-  echo -n '.'
-  [ -f "$file" ] && rm $file && i=i+1 && k=k+1
+  [ -f "$file" ] && rm $file && i=i+1 && k=k+1 && printf "-"
 done
-i=0
-echo '\n'; echo $k 'files cleaned up...\n'; k=0;
+printf '\n%d files cleaned up...\n' $k
+echo
 
-echo '///////// TRANSFERRING //////////\n'
+echo '///////// TRANSFERRING //////////'
+let i=0; let k=0
 for dir in $dirs
 do
-  echo '////////  '$dir'  //////////'
+  $VERBOSE && echo '\n////////  '$dir'  //////////'
+  $VERBOSE && i=0
   for file in $dir/*
   do
-    if [[ $i -eq 80 ]]
+    if [[ $i -eq ($(tput cols) - 1) ]]
     then
       echo
       i=0
     fi
-    echo -n '.'
     [ -f "$file" ] &&
       cp $file $slideshow &&
       sudo cp $file $shared_slideshow &&
-      i=i+1 && k=k+1
+      i=i+1 && k=k+1 &&
+      printf '+'
   done
-  echo '\n'
-  i=0
 done
-echo $k 'files transferred...\n';k=0;
+printf '\n%d files transferred...\n' $k
+
+DUPES="$(diff <(find "$slideshow" -type f -exec basename {} \; | sort) <(for dir in $dirs; do find "$dir" -type f -exec basename {} \; ; done | sort))"
+[ $DUPES == ""] || echo "Possible duplicates in filesystem:" $DUPES
+echo
